b50862d2a65c7840c714079223f2257c5173b454
Codi Kraus <codi.kraus@gmail.com>
2021-04-29 22:38:41 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,11 @@
 def prime?(num)
   if num <= 1 || num == 0
     return false
-  end(2..num - 1).each do |i|
-    if num % i == 0
-      return false
+  elsif
+    (2..num - 1).each do |i|
+      if num % i == 0
+        return false
+      end
     end
   end
 end
